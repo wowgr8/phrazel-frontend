2d451e73b8e2215a53df80dfc04694327d917065
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2023-03-26 15:00:24 -0700

    add boolean state to toggle between signup and login forms. Add Login input fields - WIP

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Login() {
+  // Used to conditionally render sign up or login form.
+  const [showSignUpForm, setShowSignUpForm] = useState(false);
+
+  
+
   return (
-    <div>Login</div>
+    <>
+      <div>{showSignUpForm ? "SIGN UP" : "Login"}</div>
+      <form >
+        <label>username: </label>
+        <input></input>
+        <label>password: </label>
+        <input></input>
+      </form>
+    </>
   )
 }
 
