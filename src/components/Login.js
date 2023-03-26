006f8bc4f4c65b3e68626b80f776c6bd1a6f5ff8
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2023-03-26 15:48:09 -0700

    add toggleForm handler to conditionally render signup form. Add sign up form fields. Add submitForm handler (WIP) to navigate to GameLobby or ProfilePage - WIP

--- src/components/Login.js
+++ src/components/Login.js
@@ -4,17 +4,50 @@ function Login() {
   // Used to conditionally render sign up or login form.
   const [showSignUpForm, setShowSignUpForm] = useState(false);
 
-  
+  const toggleForm = () => {
+    setShowSignUpForm(!showSignUpForm);
+    console.log(showSignUpForm)
+  }
+
+  const submitForm = () => {
+    if(showSignUpForm === true){
+      console.log("PROFILEPAGE")
+      // navigate to ProfilePage
+    } else {
+      setShowSignUpForm(!showSignUpForm);
+      console.log("GAMELOBBY")
+      // navigate to GameLobby 
+    }
+  }
 
   return (
-    <>
-      <div>{showSignUpForm ? "SIGN UP" : "Login"}</div>
-      <form >
-        <label>username: </label>
-        <input></input>
-        <label>password: </label>
-        <input></input>
-      </form>
+    <> 
+      {showSignUpForm 
+        ? <>
+            <h1>Sign Up</h1>
+            <form onSubmit={submitForm}>
+              <label>username: </label>
+              <input></input>
+              <label>email: </label>
+              <input></input>
+              <label>password: </label>
+              <input></input>
+              <button type="submit">Signup!</button>
+            </form>
+            <button onClick={toggleForm} >Already have an account? Sign in </button>
+          </> 
+        : <>
+            <h1>Login</h1>
+            <form onSubmit={submitForm}>
+              <label>username: </label>
+              <input></input>
+              <label>password: </label>
+              <input></input>
+              <button type="submit">Login!</button>
+            </form>
+            <button onClick={toggleForm} >Dont have an account? Register </button>
+          </> 
+      }
     </>
   )
 }
