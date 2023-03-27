9e7e3c006c9a2c0a9dc087157075186f4a4c05f3
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2023-03-27 16:44:12 -0700

    add notes and hamburgerNav() - WIP

--- src/components/GameBoard.js
+++ src/components/GameBoard.js
@@ -1,8 +1,22 @@
-import React from 'react'
+import React from 'react';
 
 function GameBoard() {
+
+  // div for user guesses - input field
+    // with submit button
+  // div for chat box
+  // div for scoreboard
+  // div for Hamburger nav button -  exits to profile page or game lobby
+  const hamburgerNav = () => {
+    console.log("Exiting to GameLobby or ProfilePage")
+  }
+
   return (
-    <div>GameBoard</div>
+    <div>
+      <div onClick={hamburgerNav}>Hamburger button</div>
+      <h1>GameBoard Placeholder</h1>
+
+    </div>
   )
 }
 
