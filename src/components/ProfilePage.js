c8b7b6c6f4ab57fa01a5906dc2af9d5edc47b66f
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2023-03-28 12:48:21 -0700

    add comments

--- src/components/ProfilePage.js
+++ src/components/ProfilePage.js
@@ -1,4 +1,10 @@
-import React from 'react'
+import React from 'react';
+
+// exit button - that takes user back to gamelobby
+  // Will need a button
+// section for profile photo
+// section for drop down - to select avatar
+// section for games w/l ratio
 
 function ProfilePage() {
   return (
