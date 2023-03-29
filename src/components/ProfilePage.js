e199990df58dbc9570f9d02410c2b76e358c7b5e
MacKenzie <mjs94080@gmail.com>
2023-03-29 09:16:16 -0700

    added more avatar options

--- src/components/ProfilePage.js
+++ src/components/ProfilePage.js
@@ -19,17 +19,21 @@ function ProfilePage() {
     <>
       <h1>ProfilePage</h1>
       <img></img>
+      <div>Avatar Dropdown Placeholder</div>
       <label for="avatar dropdown">Choose your avatar</label>
       <select name="avatar names" id="avatar id">
         <option value="avatar1">Avatar 1</option>
         <option value="avatar2">Avatar 2</option>
         <option value="avatar3">Avatar 3</option>
+        <option value="avatar4">Avatar 4</option>
+        <option value="avatar5">Avatar 5</option>
       </select>
-      <div>Avatar Dropdown Placeholder</div>
+
       <div>
         <h4>Wins/Losses</h4>
         {wins} : {losses}
       </div>
+
       <button onClick={exitProfile}>X</button>
     </>
   );
