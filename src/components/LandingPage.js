f012e13237e551923afda65b060e5d9291447972
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2023-04-04 16:28:32 -0700

    add temporary Landing Page. Add useNavigate(), Add "Play now button" and placeholder for Login.js component. - WIP

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,8 +1,25 @@
-import React from 'react'
+import React from 'react';
+import { useNavigate } from "react-router-dom";
+// add import for Login.js
 
 function LandingPage() {
+  let navigate = useNavigate(); 
+
+  // Will assign random username and send user to GameLobby
+    // Random username - WIP
+  const playNow = () => {
+    navigate('/GameLobby'); //navigate to GameLobby
+    console.log("PlayNow - inside GAMELOBBY");
+  }
+
   return (
-    <div>LandingPage</div>
+    <>
+      <div>LandingPage</div>
+      <button onClick={playNow}>
+        <p>Sign up later, PLAY NOW!!</p>
+      </button>
+      <h5>Login Page component placeholder</h5>
+    </>
   )
 }
 
