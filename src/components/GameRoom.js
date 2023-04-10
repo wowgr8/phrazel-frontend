df6c5cea299a89bf7074e5d0d3c900c13e6e4c41
Brendan O'Connor <bhoconnor@gmail.com>
2023-04-09 23:54:24 -0400

    Add socket variable, props, Room #, & Player names

--- src/components/GameRoom.js
+++ src/components/GameRoom.js
@@ -1,50 +1,63 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import ScoreBoard from './ScoreBoard';
+import ScoreBoard from "./ScoreBoard";
+import io from "socket.io-client";
 
-function GameBoard() {
-  let navigate = useNavigate(); 
+const socket = io.connect("http://localhost:3001");
+
+function GameBoard({ room, players, setPlayers }) {
+  let navigate = useNavigate();
 
   const hamburgerNav = (event) => {
-    event.target.value === 'option1' ? navigate('/ProfilePage') : navigate('/GameLobby');
-  }
+    event.target.value === "option1"
+      ? navigate("/ProfilePage")
+      : navigate("/GameLobby");
+  };
 
   const columnStyle = {
-    display: 'inline-block', // Creates column effect
-    width: '30%', // creates spacing in between text
-    verticalAlign: 'top', // each div has the same top starting point
-    };
+    display: "inline-block", // Creates column effect
+    width: "30%", // creates spacing in between text
+    verticalAlign: "top", // each div has the same top starting point
+  };
+
+  // Added by Mau & Brendan 4/9
+  useEffect(() => {
+    socket.on("players", (data) => setPlayers(data));
+  }, [socket]);
 
   return (
     <div>
       <div>
-        <select id="navOptions"  onChange={hamburgerNav}>
+        <select id="navOptions" onChange={hamburgerNav}>
           <option value="">Hamburger nav placeholder</option>
           <option value="option1">Profile Page</option>
           <option value="option2">Game Lobby</option>
         </select>
       </div>
-      
+
       <div>
-        <h1>Room Name Placeholder</h1>
+        {/* Below edited by Mau & Brendan 4/9 */}
+        <h1>You are in Room {room}</h1>
+        <h2>Current players are: {players}</h2>
       </div>
 
       <div style={columnStyle}>
         <ScoreBoard />
       </div>
 
-      <div style={columnStyle}>GameBoard placeholder
+      <div style={columnStyle}>
+        GameBoard placeholder
         <div>
           <h4>Guess input field/ form placeholder</h4>
-          <textarea placeholder='Enter guess here'></textarea>
+          <textarea placeholder="Enter guess here"></textarea>
         </div>
       </div>
 
-      <div style={columnStyle} >
+      <div style={columnStyle}>
         <h4>Chatbox placeholder</h4>
       </div>
     </div>
-  )
+  );
 }
 
-export default GameBoard
+export default GameBoard;
