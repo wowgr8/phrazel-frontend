ee9b178d19b308a663395bb467d79cbfaf007aae
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2023-04-10 21:50:43 -0700

    remove unused comments.

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -4,21 +4,14 @@ import Login from './Login';
 
 function LandingPage({ setUserName }) {
   let navigate = useNavigate(); 
-
   const [anonForm, showAnonForm] = useState(false);
 
-  // Will assign the user a random username and send the user to the GameLobby
-    // Random username - WIP
   const toggleForm = () => {
     showAnonForm(true)
-    // navigate('/GameLobby'); //navigate to GameLobby
-    // console.log("PlayNow - inside GAMELOBBY");
   }
   const handleUsernameSubmit = (event) => {
     event.preventDefault();
     setUserName(event.target.username.value);
-    console.log(event.target.username.value);
-    // console.log("PlayNow - inside GAMELOBBY");
     navigate('/GameLobby'); //navigate to GameLobby ---- add this in last.
   }
 
