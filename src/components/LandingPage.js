a50623c8b976ca0c4ab01b29fdf91446364093e8
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2023-04-10 21:48:59 -0700

    add handleUsernameSubmit() which sets user input as userName variable and navigates to GameLobby.js. Add Form input field and label to ternary operator

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -14,15 +14,29 @@ function LandingPage({ setUserName }) {
     // navigate('/GameLobby'); //navigate to GameLobby
     // console.log("PlayNow - inside GAMELOBBY");
   }
-
+  const handleUsernameSubmit = (event) => {
+    event.preventDefault();
+    setUserName(event.target.username.value);
+    console.log(event.target.username.value);
+    // console.log("PlayNow - inside GAMELOBBY");
+    navigate('/GameLobby'); //navigate to GameLobby ---- add this in last.
+  }
 
   return (
     <>
       <div>LandingPage</div>
       {anonForm 
         ?
-          <form >
-
+          <form onSubmit={handleUsernameSubmit}>
+            <label>
+              Enter a username:
+              <input
+                type="text"
+                defaultValue=""
+                name="username"
+              />
+            </label>
+            <button type="submit">Play!</button>
           </form>
         :
           <button onClick={toggleForm}>
