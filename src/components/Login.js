2412d64c78e0f9faeaf03ad0af05cb81c2b5fcce
Alejandro.pc <alejandropatinoc@gmail.com>
2023-04-11 23:46:54 -0700

    users are able to sign up and sign in. Sign in keeps users in the same page if credentials are incorrect

--- src/components/Login.js
+++ src/components/Login.js
@@ -4,7 +4,10 @@ import { useNavigate } from "react-router-dom";
 function Login() {
   // Used to conditionally render sign up or login form.
   const [showSignUpForm, setShowSignUpForm] = useState(false);
-  const [userName, setUserName] = useState(""); // define this in App.js and pass userName as props into ProfilePage,GameLobby, etc.
+  const [username, setUserName] = useState(""); // define this in App.js and pass userName as props into ProfilePage,GameLobby, etc.
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
 
   let navigate = useNavigate();
 
@@ -13,19 +16,73 @@ function Login() {
     console.log(showSignUpForm);
   };
 
-  // Will take the user to profile page if they are signing up or game lobby if they signed in.
-  const submitForm = (e) => {
+  /* sign up form for new users  */
+  async function signUpForm(e) {
+    e.preventDefault();
+    try {
+      /* connecting to backend routes */
+      const response = await fetch("http://localhost:8000/api/v1/auth/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username: username,
+          email: email,
+          password: password
+        }),
+      });
+
+      console.log(`Sign up responds with status code ${response.status}`);
+      const inputValue = e.target.username.value;
+
+      /* error handeling sign up status codes - navigating to respective page*/
+      //TODO need to allow user to sign up without an email
+      //TODO need to let user know if credentials are already taken
+      if (response.status === 201) {
+        setUserName(inputValue);
+        navigate("/GameLobby");
+      } else {
+        navigate("/"); //stay on same page
+      }
+    } catch (error) {
+      console.log("Error occurred: ", error);
+    }
+  }
+
+  /* login form for existing users */
+  async function loginForm(e) {
     e.preventDefault();
 
-    const inputValue = e.target.username.value;
-    if (showSignUpForm === true) {
-      setUserName(inputValue);
-      navigate("/ProfilePage");
-    } else {
-      setUserName(inputValue);
-      navigate("/GameLobby");
+    try {
+      /* connecting to backend routes */
+      const response = await fetch("http://localhost:8000/api/v1/auth/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username: username,
+          password: password
+        }),
+      });
+
+      console.log(`login responds with status code ${response.status}`);
+      const inputValue = e.target.username.value;
+
+      /* error handeling sign in status code - navigate to respective page*/
+      //TODO  emit an error message if the user enters the wrong username or password
+      if (response.status === 200) {
+        setUserName(inputValue);
+        navigate("/GameLobby");
+      } else {
+        navigate("/"); // stay on same page
+      }
+    } catch (error) {
+      console.log("Error occurred: ", error);
     }
-  };
+  }
+
 
   return (
     <div
@@ -34,13 +91,19 @@ function Login() {
       {showSignUpForm ? (
         <>
           <h1>Sign Up</h1>
-          <form onSubmit={(e) => submitForm(e)}>
+          <form onSubmit={(e) => signUpForm(e)}>
             <label>username: </label>
-            <input name="username"></input>
+            <input name="username" onChange={(event) => {
+              setUserName(event.target.value);
+            }}></input>
             <label>email: </label>
-            <input name="email" disabled></input>
+            <input name="email" onChange={(event) => {
+              setEmail(event.target.value);
+            }}></input>
             <label>password: </label>
-            <input name="password" disabled></input>
+            <input name="password" onChange={(event) => {
+              setPassword(event.target.value);
+            }}></input>
             <button type="submit">Signup!</button>
           </form>
           <button onClick={toggleForm}>
@@ -50,18 +113,24 @@ function Login() {
       ) : (
         <>
           <h1>Login</h1>
-          <form onSubmit={(e) => submitForm(e)}>
+          <form onSubmit={(e) => loginForm(e)}>
             <label>username: </label>
-            <input name="username"></input>
+            <input name="username" onChange={(event) => {
+              setUserName(event.target.value);
+            }}></input>
             <label>password: </label>
-            <input name="password" disabled></input>
+            <input name="password" onChange={(event) => {
+              setPassword(event.target.value);
+            }}></input>
             <button type="submit">Login!</button>
           </form>
+          <p id="message"></p>
           <button onClick={toggleForm}>Dont have an account? Register </button>
         </>
       )}
     </div>
   );
 }
+
 
 export default Login;
