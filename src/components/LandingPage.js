195668cf1af7a45e07cadea4a464a11200a4e34f
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2023-04-13 17:49:14 -0700

    add Login component to conditionally render. Add back button to show login form and hide play now field. - WIP
    Co-authored-by: mautoranzo <mautoranzo@users.noreply.github.com>

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -2,13 +2,15 @@ import React, { useState } from 'react';
 import { useNavigate } from "react-router-dom";
 import Login from './Login';
 
-function LandingPage({ setAnonymousUsername }) {
+function LandingPage({ setAnonymousUsername, anonymousUsername }) {
   let navigate = useNavigate(); 
   const [anonForm, showAnonForm] = useState(false);
+  const [playNow] = useState(false);
 
   const toggleForm = () => {
-    showAnonForm(true)
+    showAnonForm(!anonForm)
   }
+  console.log("anonymous username check in LandingPage:", anonymousUsername )
   const handleUsernameSubmit = (event) => {
     event.preventDefault();
     setAnonymousUsername(event.target.username.value);
@@ -20,24 +22,38 @@ function LandingPage({ setAnonymousUsername }) {
       <div>LandingPage</div>
       {anonForm 
         ?
-          <form onSubmit={handleUsernameSubmit}>
-            <label>
-              Enter a username:
-              <input
-                type="text"
-                defaultValue=""
-                name="username"
-              />
-            </label>
-            <button type="submit">Play!</button>
-          </form>
+          <>
+            <form onSubmit={handleUsernameSubmit}>
+              <label>
+                Enter a username:
+                <input
+                  type="text"
+                  defaultValue=""
+                  name="username"
+                />
+              </label>
+              <button type="submit">Play!</button>
+            </form>
+            <button onClick={toggleForm}>
+              <p> Back to sign in</p>
+            </button>
+          </>
         :
           <button onClick={toggleForm}>
             <p>Sign up later, PLAY NOW!!</p>
           </button>
       }
 
-      <Login />
+      {anonForm 
+        ? null
+        : <Login />
+      }
+      
+
+      {/* <div >
+        <input placeholder='User Name...' onChange={userNameHandler} />
+          <button onClick={connect}>Connect</button>
+      </div> */}
     </>
   )
 }
