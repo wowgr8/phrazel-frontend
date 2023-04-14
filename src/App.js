c3f0d96811cb556b0699fa74b10d47da023ad1c4
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2023-04-13 17:45:15 -0700

    moved socket connection to utils/socket.js and moved useEffect to GameLobby. Remove GameRoom component tag inside GameLobby.js - WIP
    Co-authored-by: mautoranzo <mautoranzo@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,98 +1,108 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useContext } from "react";
 import "./App.css";
-import io from "socket.io-client";
+// import io from "socket.io-client"; // Moved to GameLobby
 import Login from "./components/Login";
 import LandingPage from "./components/LandingPage";
 import ProfilePage from "./components/ProfilePage";
 import GameLobby from "./components/GameLobby";
 import GameRoom from "./components/GameRoom";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {SocketContext, socket} from './utils/Socket';
 
-const socket = io.connect("http://localhost:3001");
+// const socket = io.connect("http://localhost:3001"); // MOved to GameLobby
 
 function App() {
   // only used in GameLobby
-  const [availableRooms, setAvailableRooms] = useState([]); 
+  // const [availableRooms, setAvailableRooms] = useState([]);  // removed as props and created in gamelobby
 
   // used in GameRoom and GameLobby
-  const [userName, setUserName] = useState(""); 
+  // const [userName, setUserName] = useState(""); // Replaced by username usestate defined in Login.js
   const [anonymousUsername, setAnonymousUsername] = useState(""); 
-  const [inRoom, setInRoom] = useState(false);
-  const [room, setRoom] = useState("");
+  // const [inRoom, setInRoom] = useState(false); // moved to gameroom
+  // const [room, setRoom] = useState(""); // moved to gameroom
 
-  // May need to be moved to GameRoom.
-  const [players, setPlayers] = useState([]); 
-  let localStorageRoom = localStorage.getItem("room");
+  
+  // list of players inside your room. - MOVED to GameRoom
+  // const [players, setPlayers] = useState([]);
+  
+  // Below two useEffects were moved to GameLobby
 
-  useEffect(() => {
-    localStorage.setItem("room", room)
-  }, [room]);
+  // let localStorageRoom = localStorage.getItem("room");
 
-  useEffect(() => {
-    socket.on("room_number", (room) => {
-      setRoom(room) }
-      );
+  // useEffect(() => {
+  //   localStorage.setItem("room", room)
+  // }, [room]);
 
-      // If there's something in localStorageRoom, we set the room to the localStorage room upon refreshing. 
-      if(localStorageRoom) {
-        setRoom(localStorageRoom);
-      }
+  // useEffect(() => {
+  //   socket.on("room_number", (room) => {
+  //     setRoom(room) }
+  //     );
 
-    socket.on("available_rooms", (data) => {
-      console.log("inside GameLobby useEffect", data);
-      if (data === false) {
-        console.log("inside GameLobby IF", data);
-        return;
-      } else {
-        console.log("inside GameLobby ELSE", data);
-        setAvailableRooms(data);
-      }
-      console.log("Data:", data);
-    });
-  }, [socket]);
+  //     // If there's something in localStorageRoom, we set the room to the localStorage room upon refreshing. 
+  //     if(localStorageRoom) {
+  //       setRoom(localStorageRoom);
+  //     }
+
+  //   socket.on("available_rooms", (data) => {
+  //     console.log("inside GameLobby useEffect", data);
+  //     if (data === false) {
+  //       console.log("inside GameLobby IF", data);
+  //       return;
+  //     } else {
+  //       console.log("inside GameLobby ELSE", data);
+  //       setAvailableRooms(data);
+  //     }
+  //     console.log("Data:", data);
+  //   });
+  // }, [socket]);
+
+  // Callback - potentially needed here. for anonymoususername
 
 
+  console.log("anon username in app.js:", anonymousUsername);
 
   return (
-    <div className="App">
-      <Router>
-        <Routes>
-          <Route path="/" exact element={<LandingPage setAnonymousUsername={setAnonymousUsername} />} />
-          <Route path="Login" exact element={<Login />} />
-          <Route path="ProfilePage" exact element={<ProfilePage />} />
-          <Route
-            path="GameLobby"
-            exact
-            element={
-              <GameLobby
-                socket={socket}
-                setInRoom={setInRoom}
-                userName={userName}
-                anonymousUsername={anonymousUsername}
-                availableRooms={availableRooms}
-                setAvailableRooms={setAvailableRooms}
-                room={room}
-                setRoom={setRoom}
-              />
-            }
-          />
-          <Route
-            path="GameRoom"
-            exact
-            element={
-              <GameRoom 
-                socket={socket}
-                room={room} 
-                players={players} 
-                setPlayers={setPlayers} 
-                userName={userName}
-                anonymousUsername={anonymousUsername}
-              />
-            }
-          />
-        </Routes>
-      </Router>
-    </div>
+    <SocketContext.Provider value={socket}>
+      <div className="App">
+        <Router>
+          <Routes>
+            <Route path="/" exact element={<LandingPage anonymousUsername={anonymousUsername} setAnonymousUsername={setAnonymousUsername} />} />
+            <Route path="Login" exact element={<Login />} />
+            <Route path="ProfilePage" exact element={<ProfilePage />} />
+            <Route
+              path="GameLobby"
+              exact
+              element={
+                <GameLobby
+                  // socket={socket}
+                  // setInRoom={setInRoom}
+                  // userName={userName}
+                  anonymousUsername={anonymousUsername}
+                  // availableRooms={availableRooms}
+                  // setAvailableRooms={setAvailableRooms}
+                  // room={room}
+                  // setRoom={setRoom}
+                />
+              }
+            />
+            {/* <Route
+              path="GameRoom"
+              exact
+              element={
+                <GameRoom 
+                  // socket={socket}
+                  // room={room} 
+                  // players={players} 
+                  // setPlayers={setPlayers} 
+                  // userName={userName}
+                  anonymousUsername={anonymousUsername}
+                />
+              }
+            /> */}
+          </Routes>
+        </Router>
+      </div>
+    </SocketContext.Provider>
   );
 }
 
