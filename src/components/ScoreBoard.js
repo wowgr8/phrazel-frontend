418377016087c0433006e67839197bebbdaef251
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2023-04-17 16:05:02 -0700

    add socketContext, and useState for gameScore. Add socket.on inside a useEffect. Add loop through gamescore and pseudo code - WIP

--- src/components/ScoreBoard.js
+++ src/components/ScoreBoard.js
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState, useContext } from 'react';
+import { SocketContext } from '../utils/Socket';
 
 function ScoreBoard() {
+  const socket = useContext(SocketContext);
+  const [gameScore, setGameScore] = useState([]);
+
+  useEffect(() => {
+    socket.on("game_score", (gameScore) => {
+      setGameScore(gameScore);
+    })
+    console.log(gameScore)
+  }, [gameScore])
+  
+  // loop through gameScore
+    // sort gameScore by gameScore.roundsWon.
+    // list gameScore.player and gameScore.roundWon.
+
+
   return (
     <>
       <h3>ScoreBoard Component</h3>
-      <ol>
+
+      {gameScore.map((player) => {
+
+      })}
+      {/* <ol>
         <li>Player username</li>
           <ul>
             <li>Score: 50</li>
@@ -20,7 +40,7 @@ function ScoreBoard() {
             <li>Score: 30</li>
             <li>Correct Guesses: 3/10</li>
           </ul>
-      </ol>
+      </ol> */}
     </>
   )
 }
