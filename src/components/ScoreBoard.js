ef8e0f9e0ab05d3ecb732b4570027e476f11c031
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2023-04-17 20:46:14 -0700

    add players as prop. Add useState boolean and ternary operator to display all player names and scores at 0 before the round ends.

--- src/components/ScoreBoard.js
+++ src/components/ScoreBoard.js
@@ -1,31 +1,51 @@
 import React, { useEffect, useState, useContext } from 'react';
 import { SocketContext } from '../utils/Socket';
 
-function ScoreBoard() {
+function ScoreBoard({ players }) {
   const socket = useContext(SocketContext);
   const [gameScore, setGameScore] = useState([]);
+  const [showData, setShowData] = useState(false);
 
   useEffect(() => {
     socket.on("game_score", (gameScore) => {
       setGameScore(gameScore);
+      setShowData(true);
     })
+    console.log(gameScore)
+    console.log(players)
   }, [gameScore])
   
-  const sortedScores = gameScore.sort((a, b) => b.roundsWon - a.roundsWon);
-
   return (
     <>
       <h3>ScoreBoard Component</h3>
-      <div>
-        {sortedScores.map((sortedScore) => {
-          return (
-            <div key={sortedScore.player}>
-              <h6>Player Name: {sortedScore.player} </h6>
-              <h6>Score: {sortedScore.roundsWon} </h6>
-            </div>
-          )
-        })}
-      </div>
+
+      {showData ? 
+        <>
+          <div>
+            {gameScore.sort((a, b) => b.roundsWon - a.roundsWon).map((sortedScore) => {
+              return (
+                <div key={sortedScore.player}>
+                  <h6>Player Name: {sortedScore.player} </h6>
+                  <h6>Score: {sortedScore.roundsWon} </h6>
+                </div>
+              )
+            })}
+          </div>
+        </> 
+      : 
+        <>
+          <div>
+            {players.map((player) => {
+              return (
+                <div >
+                  <h6>Player Name: {player} </h6>
+                  <h6>Score: 0 </h6>
+                </div>
+              )
+            })}
+          </div>
+        </> 
+      }
     </>
   )
 }
