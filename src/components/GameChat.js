2635342ca61e4b8dd49969ffcb3863a9bf756d10
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2023-04-27 14:34:35 -0700

    add import for emoji-picker-react and emoji svg - WIP

--- src/components/GameChat.js
+++ src/components/GameChat.js
@@ -1,5 +1,7 @@
 import React, { useContext, useEffect, useState } from 'react'
 import {SocketContext} from '../utils/Socket';
+import EmojiPicker from 'emoji-picker-react';
+import {ReactComponent as HappyFaceSVG} from "../svg/happy-face.svg";
 
 function GameChat({ room, userName }) {
   const socket = useContext(SocketContext);
@@ -42,19 +44,23 @@ function GameChat({ room, userName }) {
   return (
     <div className='ml-3.5'>
       <p>GameChat in room: {room}</p>
+      <EmojiPicker height={450} className="w-full"/>
       <div className='flex flex-row mb-2.5 gap-0.5'>
         <input 
           placeholder="Message..." 
           maxLength="50"
           className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block basis-4/5 p-2.5'
           onChange={(event)=> { setMessage(event.target.value)}} 
+        
         />
+        {/* <EmojiPicker /> ADD a drawer to an emoji icon that then shows EmojiPicker*/}
         <button 
           className="text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-1 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm  py-2.5 text-center basis-1/5  "
           onClick={sendMessage}
         >
           Send
         </button>
+        <HappyFaceSVG height="30px" width="30px" />
       </div>
 
       <div style={chatBoxStyle} >
