14b51aee52b2d51cd1e62c82136d8153c2c82e89
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2023-04-26 17:19:41 -0700

    add game name

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -36,7 +36,7 @@ function LandingPage({ setUserName, userName }) {
   },[socket])
   return (
     <div className='my-56 '>
-      <>Game name placeholder</>
+      <strong>Phrazel</strong>
       <div className="container flex flex-col md:flex-row items-center px-6 mx-auto mt-10 space-y-0 md:space-y-0 bg-color-sky-300 columns-2">
         {/* Left Item */}
         <div className='flex flex-col mb-32 space-y-12 md:w-1/2'>
