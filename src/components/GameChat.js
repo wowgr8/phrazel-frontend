06d7389db99c7e19d38cef9a3cf7fcdf64b967fe
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2023-04-27 17:24:24 -0700

    add onEmojiClick handler fn to capture emoji selection. Add properties to emojiPicker, add input wipe after message submit. Add input value to show emoji selection.

--- src/components/GameChat.js
+++ src/components/GameChat.js
@@ -2,6 +2,7 @@ import React, { useContext, useEffect, useState } from 'react'
 import {SocketContext} from '../utils/Socket';
 import EmojiPicker from 'emoji-picker-react';
 import {ReactComponent as HappyFaceSVG} from "../svg/happy-face.svg";
+import {ReactComponent as SendSVG} from "../svg/send.svg";
 
 function GameChat({ room, userName }) {
   const socket = useContext(SocketContext);
@@ -10,11 +11,12 @@ function GameChat({ room, userName }) {
   const [messageReceived, setMessageReceived] = useState('');
   const [chatLog, setChatLog] = useState([]);
   const [showEmojiPicker, setShowEmojiPicker] = useState(false);
-  // const [selectedEmoji, setSelectedEmoji] = useState('');
 
   // emits message, room, and userName to the backend
   const sendMessage = () => {
     socket.emit('send_message', { message, room, userName });
+    
+    setMessage("")
   }
 
   useEffect(() => {
@@ -43,10 +45,25 @@ function GameChat({ room, userName }) {
     background: '#F0F8FF'
   };
 
+  const onEmojiClick = (emojiObject) => {
+    setMessage(message + emojiObject.emoji);
+    setShowEmojiPicker(!showEmojiPicker);
+  };
+
   return (
     <div className='ml-3.5'>
-      <p>GameChat in room: {room}</p>
-      {showEmojiPicker && <EmojiPicker height={450} className="w-full"/>}
+      {showEmojiPicker && 
+        <EmojiPicker 
+          height={300} 
+          className="w-full" 
+          searchDisabled={true}
+          // disableAutoFocus={true}
+          // disablePreview={true}
+          skinTonesDisabled={true}
+          emoji="smileys_people"
+          onEmojiClick={onEmojiClick}
+        />
+      }
       <div className='flex flex-row mb-2.5 gap-0.5'>
         <div className="flex items-center px-1 py-2 w-full rounded-lg bg-sky-100 gap-0.5">
         <input 
@@ -54,19 +71,22 @@ function GameChat({ room, userName }) {
           maxLength="50"
           className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block basis-4/5 p-2.5'
           onChange={(event)=> { setMessage(event.target.value)}} 
-        
+          value={message}
         />
         <HappyFaceSVG 
           height="30px" 
           width="30px" 
           onClick={() => setShowEmojiPicker(!showEmojiPicker)}
+          className="ml-1 hover:bg-yellow-100 rounded-full"
         />
         <button 
           type="submit" class="inline-flex justify-center p-2 text-blue-600 rounded-full cursor-pointer hover:bg-blue-100 "
-          // className="text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-1 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm  py-2.5 text-center basis-1/5  "
           onClick={sendMessage}
         >
-          <svg aria-hidden="true" className="w-6 h-6 rotate-90 " fill="current" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M10.894 2.553a1 1 0 00-1.788 0l-7 14a1 1 0 001.169 1.409l5-1.429A1 1 0 009 15.571V11a1 1 0 112 0v4.571a1 1 0 00.725.962l5 1.428a1 1 0 001.17-1.408l-7-14z"></path></svg>
+          <SendSVG 
+            height="35px" 
+            width="35px"
+          />
         </button>
         </div>
         
