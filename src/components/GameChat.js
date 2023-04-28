dcf05a69aad3aba5af7ebb042ede810ebd0512cf
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2023-04-27 18:57:45 -0700

    replace chatBoxStyling with tailwind inline styling. Add opacity and dropshadow on hover to chatbox

--- src/components/GameChat.js
+++ src/components/GameChat.js
@@ -36,22 +36,13 @@ function GameChat({ room, userName }) {
     }
   }, [messageReceived]);
 
-  const chatBoxStyle = {
-    border: "1px solid #ADD8E6", 
-    height: "400px",
-    overflowY: "scroll",
-    borderRadius: "15px",
-    boxShadow: "0 0 5px 0 rgba(0, 0, 0, 0.2)",
-    background: '#F0F8FF'
-  };
-
   const onEmojiClick = (emojiObject) => {
     setMessage(message + emojiObject.emoji);
     setShowEmojiPicker(!showEmojiPicker);
   };
 
   return (
-    <div className='ml-3.5'>
+    <div className='ml-3.5 opacity-90'>
       {showEmojiPicker && 
         <div className='flex justify-center'>
           <EmojiPicker 
@@ -64,50 +55,54 @@ function GameChat({ room, userName }) {
           />
         </div>
       }
-      <div className='flex flex-row mb-2.5 gap-0.5'>
-        <div className="flex items-center px-1 py-2 w-full rounded-lg bg-sky-100 gap-0.5">
-        <input 
-          placeholder={"Message... " }
-          maxLength="50"
-          className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block basis-4/5 p-2.5'
-          onChange={(event)=> { setMessage(event.target.value)}} 
-          value={message}
-        />
-        <HappyFaceSVG 
-          height="30px" 
-          width="30px" 
-          onClick={() => setShowEmojiPicker(!showEmojiPicker)}
-          className="ml-1 hover:bg-yellow-100 rounded-full"
-        />
-        <button 
-          type="submit" class="inline-flex justify-center p-2 text-blue-600 rounded-full cursor-pointer hover:bg-blue-100 "
-          onClick={sendMessage}
-        >
-          <SendSVG 
-            height="35px" 
-            width="35px"
+      <div id="chatbox"className="hover:shadow-xl hover:shadow-cyan-500/50">
+        <div className='flex flex-row mb-2.5 gap-0.5'>
+          <div className="flex items-center px-1 py-2 w-full rounded-lg bg-sky-100 gap-0.5">
+          <input 
+            placeholder={"Message... " }
+            maxLength="50"
+            className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block basis-4/5 p-2.5'
+            onChange={(event)=> { setMessage(event.target.value)}} 
+            value={message}
           />
-        </button>
+          <HappyFaceSVG 
+            height="30px" 
+            width="30px" 
+            onClick={() => setShowEmojiPicker(!showEmojiPicker)}
+            className="ml-1 hover:bg-yellow-100 rounded-full"
+          />
+          <button 
+            type="submit" class="inline-flex justify-center p-2 text-blue-600 rounded-full cursor-pointer hover:bg-blue-100 "
+            onClick={sendMessage}
+          >
+            <SendSVG 
+              height="35px" 
+              width="35px"
+            />
+          </button>
+          </div>
+          
         </div>
-        
+
+        <div  className='border border-blue-200 h-96 overflow-y-scroll rounded-lg shadow-md bg-blue-50 '>
+          <div >
+            {chatLog.map((message, index) => (
+              <p 
+                key={index} 
+                className="text-left ml-2.5 text-2xl"
+                style={{ 
+                  color: message.userName === userName ? '#ECBE07' : 'black', 
+                  padding: '10px' 
+                }}  
+              > 
+                <strong>{message.userName}</strong>: {message.message}
+              </p>
+            ))}
+          </div>
+        </div>
       </div>
 
-      <div style={chatBoxStyle} >
-        <div >
-          {chatLog.map((message, index) => (
-            <p 
-              key={index} 
-              className="text-left ml-2.5 text-2xl"
-              style={{ 
-                color: message.userName === userName ? '#ECBE07' : 'black', 
-                padding: '10px' 
-              }}  
-            > 
-              <strong>{message.userName}</strong>: {message.message}
-            </p>
-          ))}
-        </div>
-      </div>
+
     </div>
   )
 }
