5b7713f2c999737c70a9274528e8a1033c81709c
Brendan O'Connor <bhoconnor@gmail.com>
2023-04-29 18:34:39 -0400

    Move loop of available rooms into AvailableRooms.js
    Co-authored-by: Cesar Lopez <wowgr8@users.noreply.github.com>
    
    Co-authored-by: MacKenzie Santiago <mjs94080@gmail.com>

--- src/components/GameLobby.js
+++ src/components/GameLobby.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect, useContext } from "react";
 import GameRoom from "./GameRoom";
 import { useNavigate } from "react-router-dom";
 import { SocketContext } from "../utils/Socket";
+import AvailableRooms from "./AvailableRooms";
 
 function GameLobby({ userName, gamesWon, _id }) {
   const socket = useContext(SocketContext);
@@ -112,50 +113,11 @@ function GameLobby({ userName, gamesWon, _id }) {
 
                 <h2>Join a Room!</h2>
                 <h3>The available rooms & the players within them are:</h3>
-                <ul>
-                  {availableRooms.map((roomDetails) => (
-                    <div key={roomDetails.roomNumber}>
-                      <br></br>
-
-                      <li
-                        style={{
-                          marginLeft: "30%",
-                          marginRight: "55%",
-                          textAlign: "left",
-                        }}
-                      >
-                        <strong>Room: {roomDetails.roomNumber}</strong>
-                      </li>
-                      <ul>
-                        <li
-                          key={roomDetails.players}
-                          style={{
-                            marginLeft: "35%",
-                            marginRight: "25%",
-                            textAlign: "left",
-                          }}
-                        >
-                          <u>Players</u>: {roomDetails.players.join(", ")}
-                        </li>
-                      </ul>
-                    </div>
-                  ))}
-                </ul>
-              </div>
-              <div className="justify-center items-center">
-                <input
-                  placeholder="Enter Room to Join..."
-                  style={{
-                    margin: "auto",
-                    display: "block",
-                  }}
-                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-1.2 p-2.5"
-                  onChange={(event) => {
-                    handleSetRoom(event);
-                  }}
+                <AvailableRooms
+                  availableRooms={availableRooms}
+                  joinRoom={joinRoom}
+                  handleSetRoom={handleSetRoom}
                 />
-
-                <button onClick={joinRoom}>Join</button>
               </div>
             </div>
           )}
