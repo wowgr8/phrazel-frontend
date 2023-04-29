4ed7a1ed2d7803ff60e512cd2f34ad3c5acdbcd6
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2023-04-28 18:30:12 -0700

    add react-timer-hook, add Date object and add 10 seconds. - WIP

--- src/components/RoundCountDown.js
+++ src/components/RoundCountDown.js
@@ -1,9 +1,35 @@
-import React from 'react'
+import React from "react";
+import { useTimer } from 'react-timer-hook';
 
 function RoundCountDown() {
+
+  const time = new Date();
+  console.log(time)
+  time.setSeconds(time.getSeconds() + 10);
+
+  //Timer is working.. 
+  // Pass timer back up to GameRoom and have it run upon selecting "start game" button....
+  
+  const {
+    seconds,
+    minutes,
+    hours,
+    days,
+    isRunning,
+    start,
+    pause,
+    resume,
+    restart,
+  } = useTimer({ expiryTimestamp: time });
+
   return (
-    <div>RoundCountDown</div>
-  )
+    <div>
+      <div style={{fontSize: '100px'}}>
+        <span>{seconds}</span>
+      </div>
+      <p>{isRunning ? 'Running' : 'Not running'}</p>
+    </div>
+  );
 }
 
-export default RoundCountDown
+export default RoundCountDown;
