676d42f2ab443a6a39c34101f9cc9a159c49d740
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2023-04-28 21:41:53 -0700

    remove react-timer-hook syntax. Add startTimer prop. Add useEffect handling setInterval and clearInterval. Add displayTime variable to nicely show timer

--- src/components/RoundCountDown.js
+++ src/components/RoundCountDown.js
@@ -1,28 +1,32 @@
-import React from "react";
-import { useTimer } from 'react-timer-hook';
+import React, { useState, useEffect } from 'react';
 
-function RoundCountDown() {
+function RoundCountDown({ startTimer }) {
+  const [seconds, setSeconds] = useState(30);
 
-  const time = new Date();
-  console.log(time)
-  time.setSeconds(time.getSeconds() + 10);
+  useEffect(() => {
+    let interval = null;
 
-  //Timer is working.. 
-  // Pass timer back up to GameRoom and have it run upon selecting "start game" button....
-  
-  const {
-    seconds,
-    isRunning
-  } = useTimer({ expiryTimestamp: time });
+    if (startTimer) {
+      interval = setInterval(() => {
+        setSeconds(seconds => seconds - 1);
+      }, 1000);
+    }
 
+    if (seconds === 0) {
+      clearInterval(interval);
+    }
+
+    return () => clearInterval(interval);
+  }, [startTimer, seconds]);
+
+  const displayTime = `${Math.floor(seconds / 60) // rounds down to the nearest integer to get the number of minutes left in the countdown
+    .toString()                                     //padStart adds a 0 to the front of a single digit integer
+    .padStart(2, '0')}:${(seconds % 60).toString().padStart(2, '0')}`;
+                          // ^^  gets the remainder of the seconds state divided by 60 to get the number of seconds left in the countdown.
   return (
-    <div>
-      <div style={{fontSize: '100px'}}>
-        <span>{seconds}</span>
-      </div>
-      <p>{isRunning ? 'Running' : 'Not running'}</p>
-    </div>
-  );
+    <div>{displayTime}</div>
+  )
+    
 }
 
 export default RoundCountDown;
