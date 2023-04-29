e273540791ace0517c30e574652bf30d09936524
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2023-04-28 22:07:23 -0700

    add light blue background, hover drop shadow to scoreboard component

--- src/components/ScoreBoard.js
+++ src/components/ScoreBoard.js
@@ -13,19 +13,11 @@ function ScoreBoard({ players }) {
     });
   }, [gameScore]);
 
-  // Attempted separate columns for players & their score in scoreboard, so wouldn't have to also have list of players above in GameRoom file (not working yet, but Brendan & Cesar will work on Tues.--also commented out below)
-
-  // const miniColumn = {
-  //   display: "inline-block", // Creates column effect
-  //   width: "15%", // creates spacing in between text
-  //   verticalAlign: "top", // each div has the same top starting point
-  // };
-
   return (
-    <>
+    <div className="border border-blue-200 h-96 overflow-y-scroll rounded-lg shadow-md bg-blue-50 mr-2.5 hover:shadow-xl hover:shadow-cyan-500/50">
       <h3>ScoreBoard Component</h3>
 
-      {showData ? (
+      {showData ? 
         <>
           <div>
             {gameScore
@@ -39,26 +31,8 @@ function ScoreBoard({ players }) {
                 );
               })}
           </div>
-          {/* Attempt to put in name & score in separate columns--see comment above for miniColumn variable for context
-          
-          <div>
-            {gameScore
-              .sort((a, b) => b.roundsWon - a.roundsWon)
-              .map((sortedScore) => {
-                return (
-                  <>
-                    <div style={miniColumn} key={sortedScore.player}>
-                      <h6>Player Name: {sortedScore.player} </h6>
-                    </div>
-                    <div style={miniColumn}>
-                      <h6>Score: {sortedScore.roundsWon} </h6>
-                    </div>
-                  </>
-                );
-              })}
-          </div> */}
         </>
-      ) : (
+      : 
         <>
           <div>
             {players.map((player) => {
@@ -71,8 +45,8 @@ function ScoreBoard({ players }) {
             })}
           </div>
         </>
-      )}
-    </>
+      }
+    </div>
   );
 }
 
