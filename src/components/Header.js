fa3e08e5ea449017382b9c191b4ae8b6057af08d
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2023-05-01 19:55:04 -0700

    add nav tag with styling, logo placeholder, and app name.

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,16 @@ import React from 'react'
 
 function Header() {
   return (
-    <div>Header</div>
+    <nav class="bg-white border-gray-200 dark:bg-gray-900">
+      <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+        {/* <a> might be unnecessary */}
+        <a href="" class="flex items-center">
+            <img src="" class="h-8 mr-3" alt="Phrazel Logo" />
+            <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">PHRAZEL GFont placeholder</span>
+        </a>
+        
+      </div>
+    </nav>
   )
 }
 
