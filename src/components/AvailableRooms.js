eba1e4bf7e79c0291ee586b0b350dcbeb8c2e683
Brendan O'Connor <bhoconnor@gmail.com>
2023-05-03 16:47:53 -0400

    Add Join button styling in AvailableRooms.js

--- src/components/AvailableRooms.js
+++ src/components/AvailableRooms.js
@@ -16,7 +16,12 @@ function AvailableRooms({ availableRooms, joinRoom, handleSetRoom }) {
             handleSetRoom(event);
           }}
         />
-        <button onClick={joinRoom}>Join</button>
+        <button
+          className="text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+          onClick={joinRoom}
+        >
+          Join
+        </button>
       </div>
 
       {/* Div for cards with room # AND players */}
@@ -30,10 +35,8 @@ function AvailableRooms({ availableRooms, joinRoom, handleSetRoom }) {
           >
             <div class="flex items-center justify-between mb-4">
               <div class="text-xl font-bold leading-none text-gray-900 dark:text-white">
-                <strong>
-                  Room:
-                  {roomDetails.roomNumber}
-                </strong>
+                <strong>Room: </strong>
+                {roomDetails.roomNumber}
               </div>
             </div>
 
