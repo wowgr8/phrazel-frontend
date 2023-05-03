ea58db44bf221244e5e8220db1d8b7843f506ce3
Brendan O'Connor <bhoconnor@gmail.com>
2023-05-03 17:18:59 -0400

    Update input box styling & placeholder text

--- src/components/AvailableRooms.js
+++ src/components/AvailableRooms.js
@@ -6,9 +6,12 @@ function AvailableRooms({ availableRooms, joinRoom, handleSetRoom }) {
       {/* Div for join room input & button */}
       <div className="justify-center items-center">
         <input
-          placeholder="Enter Room to Join..."
+          class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          placeholder="Enter Room to Join from Below..."
           style={{
             margin: "auto",
+            width: "250px",
+            padding: "12px 20px",
             display: "block",
           }}
           className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-1.2 p-2.5"
