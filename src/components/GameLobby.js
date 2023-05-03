1e78304c788b5542e067cee709b0bf4ef38ae989
Brendan O'Connor <bhoconnor@gmail.com>
2023-05-03 16:51:47 -0400

    Update Create button styling in GameLobby.js

--- src/components/GameLobby.js
+++ src/components/GameLobby.js
@@ -66,7 +66,12 @@ function GameLobby({ userName, gamesWon, _id }) {
                 {/* //////////////////////////////////// */}
                 <h2>Create a room!</h2>
 
-                <button onClick={createRoom}>Create</button>
+                <button
+                  className="text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+                  onClick={createRoom}
+                >
+                  Create
+                </button>
 
                 <br></br>
                 {/* //////////////////////////////////// */}
@@ -95,7 +100,12 @@ function GameLobby({ userName, gamesWon, _id }) {
                 {/* //////////////////////////////////// */}
                 <h2>Create a room!</h2>
 
-                <button onClick={createRoom}>Create</button>
+                <button
+                  className="text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+                  onClick={createRoom}
+                >
+                  Create
+                </button>
 
                 <br></br>
               </div>
