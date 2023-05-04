e4d3362e0968b1adec7d4c6f9e0096cb0af38bcf
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2023-05-04 15:40:50 -0700

    replace white bg's with dark mode as colors as default

--- src/components/Header.js
+++ src/components/Header.js
@@ -12,7 +12,7 @@ function Header({ setInRoom, setShowHeader }) {
   };
 
   return (
-    <nav className="bg-white border-gray-200 dark:bg-gray-900">
+    <nav className="border-gray-200 bg-gray-900">
       <div className="flex flex-wrap items-center justify-between mx-auto p-4">
         <div className="flex items-center">
             {/* <img src="" className="h-8 mr-3" alt="Phrazel Logo" /> */}
@@ -20,7 +20,7 @@ function Header({ setInRoom, setShowHeader }) {
         </div>
 
         <div className="hidden w-full md:block md:w-auto" id="navbar-default">
-          <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+          <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border rounded-lg md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-gray-900 bg-gray-800 border-gray-700">
             {/* <li>
               <NavLink 
                 to='/' 
@@ -31,14 +31,14 @@ function Header({ setInRoom, setShowHeader }) {
             <li>
               <NavLink 
                 to='/ProfilePage' 
-                className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 hover:text-blue-700 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent" aria-current="page">
+                className="block py-2 pl-3 pr-4 rounded hover:bg-gray-100 hover:text-blue-700 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 text-white md:hover:text-blue-500 hover:bg-gray-700 hover:text-white md:hover:bg-transparent" aria-current="page">
                   Profile
               </NavLink>
             </li>
             <li>
               <button 
                 onClick={disconnectRoom} 
-                className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
+                className="block py-2 pl-3 pr-4 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:p-0 text-white md:hover:text-blue-500 hover:bg-gray-700 hover:text-white md:hover:bg-transparent">
                   Logout
               </button>
             </li>
