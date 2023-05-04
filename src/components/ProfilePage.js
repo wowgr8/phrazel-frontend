8435a1dad454ec55d80675e8fcd885c0c7ad32a5
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2023-05-04 16:54:25 -0700

    remove commented code. Add showUpdateForm boolean state and ternary operator. Add openForm() and submitChanges()

--- src/components/ProfilePage.js
+++ src/components/ProfilePage.js
@@ -1,87 +1,48 @@
 import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
 
 function ProfilePage() {
   const [winLoseCount, setwinLoseCount] = useState({ wins: 0, lose: 0 });
+  const [showUpdateForm, setShowUpdateForm] = useState(false);
 
-  //let wins = 0; useState
-  //wins = 1; setWins
-  //<div>{wins}</div> (displays value of wins) jsx
-
-  let navigate = useNavigate();
-
-  // function exitProfile(event) {
-  //   event.target.value === "game lobby"
-  //     ? navigate("/GameLobby")
-  //     : navigate("/GameRoom");
-  // }
+  const openForm = () => {
+    setShowUpdateForm(true)
+  }
+  const submitChanges = () => {
+    setShowUpdateForm(false)
+  }
 
   return (
     <div>
-      <h1>ProfilePage</h1>
-
-      {/* <!-- Modal toggle --> */}
-      {/* <button data-modal-target="defaultModal" data-modal-toggle="defaultModal" class="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">
-        Toggle modal
-      </button> */}
-
-      {/* <!-- Main modal --> */}
-      <div >
-        <p>HELLO</p>
-          {/* <div class="relative w-full max-w-2xl max-h-full">
-              <!-- Modal content -->
-              <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
-                  <!-- Modal header -->
-                  <div class="flex items-start justify-between p-4 border-b rounded-t dark:border-gray-600">
-                      <h3 class="text-xl font-semibold text-gray-900 dark:text-white">
-                          Profile
-                      </h3>
-                      <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="defaultModal">
-                          <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-                          <span class="sr-only">Close modal</span>
-                      </button>
-                  </div>
-                  <!-- Modal body -->
-                  <div class="p-6 space-y-6">
-                      <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-                          With less than a month to go before the European Union enacts new consumer privacy laws for its citizens, companies around the world are updating their terms of service agreements to comply.
-                      </p>
-                      <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-                          The European Union’s General Data Protection Regulation (G.D.P.R.) goes into effect on May 25 and is meant to ensure a common set of data rights in the European Union. It requires organizations to notify users as soon as possible of high-risk data breaches that could personally affect them.
-                      </p>
-                  </div>
-                  <!-- Modal footer -->
-                  <div class="flex items-center p-6 space-x-2 border-t border-gray-200 rounded-b dark:border-gray-600">
-                      <button data-modal-hide="defaultModal" type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">I accept</button>
-                      <button data-modal-hide="defaultModal" type="button" class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">Decline</button>
-                  </div>
+      <>NAME</>
+      <p>Wins:</p>
+      
+      {showUpdateForm ?
+        <>
+          <form>
+            <div class="mb-6">
+              <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
+              <input type="email" id="email" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" placeholder="name@flowbite.com" required />
+            </div>
+            <div class="mb-6">
+              <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your password</label>
+              <input type="password" id="password" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" required />
+            </div>
+            <div class="mb-6">
+              <label for="repeat-password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Repeat password</label>
+              <input type="password" id="repeat-password" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" required />
+            </div>
+            <div class="flex items-start mb-6">
+              <div class="flex items-center h-5">
+                <input id="terms" type="checkbox" value="" class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800" required />
               </div>
-          </div>                */}
-      </div>
+              <label for="terms" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">I agree with the <a href="#" class="text-blue-600 hover:underline dark:text-blue-500">terms and conditions</a></label>
+            </div>
+            <button onClick={submitChanges} type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Update</button>
+          </form>
+        </>
 
-
-      {/* <label htmlFor="hamburger dropdown"></label>
-      <select name="hamburger menu" id="navigation id" onChange={exitProfile}>
-        <option>Hamburger Menu</option>
-        <option value="game lobby">Game Lobby</option>
-        <option value="game room">Game Room</option>
-      </select> */}
-
-      {/* <img></img>
-      <div>Avatar Dropdown Placeholder</div>
-      <label htmlFor="avatar dropdown">Choose your avatar</label>
-      <select name="avatar names" id="avatar id">
-        <option value="avatar1">Avatar 1</option>
-        <option value="avatar2">Avatar 2</option>
-        <option value="avatar3">Avatar 3</option>
-        <option value="avatar4">Avatar 4</option>
-        <option value="avatar5">Avatar 5</option>
-      </select>
-
-      <div>
-        <h4>wins/lose</h4>
-        {winLoseCount.wins} : {winLoseCount.lose}
-      </div> */}
+      : <button onClick={openForm}>Reset password</button>
+      }   
     </div>
   );
 }
