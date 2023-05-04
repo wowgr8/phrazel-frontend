4eae0bd7d29dae5d837833539d4a3bf32f615419
Brendan O'Connor <bhoconnor@gmail.com>
2023-05-04 00:37:39 -0400

    Add overflow scrollbar as needed to cards

--- src/components/AvailableRooms.js
+++ src/components/AvailableRooms.js
@@ -33,7 +33,7 @@ function AvailableRooms({ availableRooms, joinRoom, handleSetRoom }) {
         {availableRooms.map((roomDetails) => (
           // Div for mapping each card, includes card styling
           <div
-            class="w-1/5 max-w-md p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700 m-1"
+            class="w-1/5 max-w-md p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700 m-1  overflow-auto"
             style={{ height: "260px" }}
             key={roomDetails.roomNumber}
           >
