1f18533f663b9163119b0ebee3d6591a334fb162
Brendan O'Connor <bhoconnor@gmail.com>
2023-05-03 23:28:55 -0400

    Increase size & add styling for Game Lobby title

--- src/components/GameLobby.js
+++ src/components/GameLobby.js
@@ -58,7 +58,9 @@ function GameLobby({ userName, gamesWon, _id, setInRoom, inRoom }) {
           {availableRooms.length === 0 ? (
             <div>
               <title>Game Lobby</title>
-              <h1>Game Lobby</h1>
+              <h1 class="mb-4 mt-2 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-gray">
+                Game Lobby
+              </h1>
               <div>
                 {/* //////////////////////////////////// */}
                 {/* Create a room section */}
