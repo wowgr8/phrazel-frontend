50aacb69732723869384961598b7665e85b413ea
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2023-05-04 16:29:53 -0700

    add profilePage import, remove NavLInk, create isProfileModalOpen boolean useState, openProfileModal() and closeProfileModal(). Add modal functionality, styling is WIP.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,25 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import { SocketContext } from "../utils/Socket";
+import ProfilePage from './ProfilePage';
 
 function Header({ setInRoom, setShowHeader }) {
   const socket = useContext(SocketContext);
+
+  const [isProfileModalOpen, setIsProfileModalOpen] = useState(false);
 
   const disconnectRoom = () => {
     socket.disconnect();
     setInRoom(false);
     setShowHeader(false)
+  };
+
+  const openProfileModal = () => {
+    setIsProfileModalOpen(true);
+  };
+
+  const closeProfileModal = () => {
+    setIsProfileModalOpen(false);
   };
 
   return (
@@ -21,30 +32,39 @@ function Header({ setInRoom, setShowHeader }) {
 
         <div className="hidden w-full md:block md:w-auto" id="navbar-default">
           <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border rounded-lg md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-gray-900 bg-gray-800 border-gray-700">
-            {/* <li>
-              <NavLink 
-                to='/' 
-                className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500" aria-current="page">
-                  Home
-              </NavLink>
-            </li> */}
             <li>
-              <NavLink 
-                to='/ProfilePage' 
-                className="block py-2 pl-3 pr-4 rounded hover:bg-gray-100 hover:text-blue-700 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 text-white md:hover:text-blue-500 hover:bg-gray-700 hover:text-white md:hover:bg-transparent" aria-current="page">
-                  Profile
-              </NavLink>
+              {/* <!-- Modal Toggle --> */}
+              <button onClick={openProfileModal} className="block py-2 pl-3 pr-4 rounded hover:bg-gray-100 hover:text-blue-700 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 text-white md:hover:text-blue-500 hover:bg-gray-700 hover:text-white md:hover:bg-transparent">
+                Profile
+              </button>
             </li>
             <li>
-              <button 
-                onClick={disconnectRoom} 
-                className="block py-2 pl-3 pr-4 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:p-0 text-white md:hover:text-blue-500 hover:bg-gray-700 hover:text-white md:hover:bg-transparent">
-                  Logout
+              <button onClick={disconnectRoom} className="block py-2 pl-3 pr-4 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:p-0 text-white md:hover:text-blue-500 hover:bg-gray-700 hover:text-white md:hover:bg-transparent">
+                Logout
               </button>
             </li>
           </ul>
-        </div>
+        </div>        
       </div>
+
+      {/* <!-- Modal content --> */}
+      {isProfileModalOpen && (
+        <div className="fixed inset-0 z-50 flex items-center justify-center overflow-x-hidden overflow-y-auto">
+          <div className="fixed inset-0 transition-opacity bg-gray-500 bg-opacity-30" onClick={closeProfileModal}></div>
+          {/* <!-- Modal body --> */}
+          <div className="relative z-50 bg-gray-600 rounded-lg px-80 py-80">
+            <div className="absolute top-0 right-0">
+              <button onClick={closeProfileModal} className="text-gray-200 hover:text-gray-700">
+                <span className="sr-only">Close</span>
+                <svg className="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
+                </svg>
+              </button>
+            </div>
+            <ProfilePage />
+          </div>
+        </div>
+      )}
     </nav>
   )
 }
