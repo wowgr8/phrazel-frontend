04d15584096ad8e01434aa2ab26e1d7490c5b1b9
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2023-05-04 15:33:07 -0700

    adjust justified-between content

--- src/components/Header.js
+++ src/components/Header.js
@@ -13,7 +13,7 @@ function Header({ setInRoom, setShowHeader }) {
 
   return (
     <nav className="bg-white border-gray-200 dark:bg-gray-900">
-      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+      <div className="flex flex-wrap items-center justify-between mx-auto p-4">
         <div className="flex items-center">
             {/* <img src="" className="h-8 mr-3" alt="Phrazel Logo" /> */}
             <span  id="logo-nav-font" className="self-center text-4xl whitespace-nowrap text-sky-400/75 ">PHRAZ_L</span>
