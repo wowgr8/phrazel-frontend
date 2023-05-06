f9f22ab02f69dfdf540c8bd71b314dac9d56d9e2
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2023-05-06 14:04:52 -0700

    remove unused fields. Add userName and gamesWon as props. add if/else statement to submitChanges() - WIP

--- src/components/ProfilePage.js
+++ src/components/ProfilePage.js
@@ -1,7 +1,7 @@
 import React, { useState, useContext } from "react";
 import {SocketContext} from '../utils/Socket';
 
-function ProfilePage() {
+function ProfilePage({ gamesWon, userName }) {
   const socket = useContext(SocketContext);
 
   const [showUpdateForm, setShowUpdateForm] = useState(false);
@@ -15,18 +15,31 @@ function ProfilePage() {
     setShowUpdateForm(true)
   }
   const submitChanges = () => {
-    socket.emit("")
-    setShowUpdateForm(false)
+    // Create socket.on() for backend with same string.
+
+    if (cycleUpdateForm) {
+      socket.emit("update password", {
+        currentPassword,
+        updatedPassword
+      })
+      setShowUpdateForm(false)
+    } else {
+      socket.emit("update userName", {
+        currentUserName,
+        updatedUserName
+      })
+      setShowUpdateForm(false)
+    }
   }
+
   const cycleForm = () => {
     setCycleUpdateForm(!cycleUpdateForm)
   }
 
   return (
-    <div>
-      {/* pass in userName and gamesWon=userData.gamesWon from App.js */}
-      <>NAME</>
-      <p>Total Wins:</p> 
+    <div className="">
+      <div className="text-xl uppercase" style={{color:"#ECBE07"}}>{userName}</div>
+      <p>Total Wins: {gamesWon}</p> 
 
       {showUpdateForm ?
         <>
@@ -34,19 +47,12 @@ function ProfilePage() {
             <>
             <form>
               <div class="mb-6">
-                <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your Emmail</label>
-                <input type="email" id="email" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" placeholder="name@flowbite.com" required />
+                <label for="new-password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">New password</label>
+                <input type="password" id="new-password" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" required />
               </div>
               <div class="mb-6">
-                <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your password</label>
+                <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Current password</label>
                 <input type="password" id="password" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" required />
-              </div>
-              <div class="mb-6">
-                <label for="repeat-password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Repeat password</label>
-                <input type="password" id="repeat-password" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" required />
-              </div>
-              <div class="flex items-start mb-6">
-                
               </div>
               <button onClick={submitChanges} type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Update</button>
             </form>
@@ -58,19 +64,12 @@ function ProfilePage() {
             <div>
               <form>
                 <div class="mb-6">
-                  <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-                  <input type="email" id="email" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" placeholder="name@flowbite.com" required />
+                  <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">New Username</label>
+                  <input type="email" id="email" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" placeholder="" required />
                 </div>
                 <div class="mb-6">
-                  <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your password</label>
+                  <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Current Email</label>
                   <input type="password" id="password" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" required />
-                </div>
-                <div class="mb-6">
-                  <label for="repeat-password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Repeat password</label>
-                  <input type="password" id="repeat-password" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" required />
-                </div>
-                <div class="flex items-start mb-6">
-                  
                 </div>
                 <button onClick={submitChanges} type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Update</button>
               </form>
@@ -79,10 +78,14 @@ function ProfilePage() {
               </button>
             </div>
           }
-
         </>
       : 
-        <button onClick={openForm}>Change Username or Password</button>
+        <button 
+          className="text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mt-10 mb-2"
+          onClick={openForm}
+        >
+          Update Username or Password
+        </button>
       }   
     </div>
   );
