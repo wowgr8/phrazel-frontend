f2e2c2a09fdc9594b5253d9c3d28e0a98ebc9020
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2023-05-07 10:37:28 -0700

    add clickable rooms to join. remove room number input field and button. Add hover effect

--- src/components/AvailableRooms.js
+++ src/components/AvailableRooms.js
@@ -1,39 +1,16 @@
 import React from "react";
 
-function AvailableRooms({ availableRooms, joinRoom, handleSetRoom }) {
+function AvailableRooms({ availableRooms, joinRoom }) {
   return (
     <div>
-      {/* Div for join room input & button */}
-      <div className="justify-center items-center">
-        <input
-          class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-          placeholder="Enter Room to Join from Below..."
-          style={{
-            margin: "auto",
-            width: "250px",
-            padding: "12px 20px",
-            display: "block",
-          }}
-          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-1.2 p-2.5"
-          onChange={(event) => {
-            handleSetRoom(event);
-          }}
-        />
-        <button
-          className="text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mt-4 mb-6"
-          onClick={joinRoom}
-        >
-          Join a Room!
-        </button>
-      </div>
-
       {/* Div for cards with room # AND players */}
       <div className="container" class="flex flex-wrap justify-center">
         {/* This map should make a new card for each room */}
         {availableRooms.map((roomDetails) => (
           // Div for mapping each card, includes card styling
           <div
-            class="w-1/5 max-w-md p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700 m-1"
+            onClick={() => joinRoom(roomDetails.roomNumber)}
+            class="w-1/5 max-w-md p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700 m-1 hover:shadow-xl hover:shadow-cyan-500/50"
             key={roomDetails.roomNumber}
           >
             <div class="flex items-center justify-between mb-4">
