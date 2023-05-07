5abf3a1f387d7c00eb5072300096ed3dc923890f
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2023-05-06 21:52:13 -0700

    remove type=password from username input field

--- src/components/ProfilePage.js
+++ src/components/ProfilePage.js
@@ -70,8 +70,8 @@ function ProfilePage({ gamesWon, userName }) {
             <div>
               <form onSubmit={(e)=>submitChanges(e)}>
                 <div class="mb-6">
-                  <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">New Username</label>
-                  <input type="password" id="pword" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" placeholder="" required onChange={(e) => {setUpdatedUserName(e.target.value)}} />
+                  <label for="newUsername" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">New Username</label>
+                  <input id="newUsername" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" placeholder="" required onChange={(e) => {setUpdatedUserName(e.target.value)}} />
                 </div>
                 <div class="mb-6">
                   <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Current Email</label>
