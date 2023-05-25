1f7a303296a7d047f96f337380828d95fb5e75ae
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2023-05-25 16:32:03 -0700

    add test commit

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -78,7 +78,7 @@ function LandingPage({ setUserName, userName, setSubmitted }) {
               </div>
               :
               <button className="text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-1 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-3 py-2.5 text-center mr-2 " onClick={toggleForm}>
-                <p>Sign up later, PLAY NOW!!</p>
+                <p>Sign up later, PLAY NOW!!!</p>
               </button>
             }
 
